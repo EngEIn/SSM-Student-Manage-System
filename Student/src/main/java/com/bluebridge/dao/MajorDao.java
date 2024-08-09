21922fa4651b3ce41e38eb690c640cefb8f1c820
EngEIn <pyx672@126.com>
2024-08-09 21:43:04 +0800

    位于分支 master
     要提交的变更：
    
    	新文件：   .vscode/settings.json
    
    	修改：     Student/pom.xml
    
    	新文件：   Student/src/main/java/com/bluebridge/dao/MajorDao.java
    
    	新文件：   Student/src/main/java/com/bluebridge/dao/MajorDao.xml
    
    	新文件：   Student/src/main/java/com/bluebridge/pojo/Major.java
    
    	新文件：   Student/target/classes/com/bluebridge/dao/MajorDao.class
    
    	新文件：   Student/target/classes/com/bluebridge/dao/MajorDao.xml
    
    	新文件：   Student/target/classes/com/bluebridge/pojo/Major.class

---
+++ Student/src/main/java/com/bluebridge/dao/MajorDao.java
@@ -0,0 +1,14 @@
+package com.bluebridge.dao;
+
+import com.bluebridge.pojo.Major;
+
+import java.util.List;
+
+public class MajorDao {
+    
+    /**
+     * 查询所有专业
+     * @return
+     */
+    List<Major> selectMajors();
+}
